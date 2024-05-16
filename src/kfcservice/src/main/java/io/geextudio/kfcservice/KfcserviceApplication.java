187f75a1bf9b8a9c7e3e95d23909fea36a09e22e
Yang SHEN <geextudio@outlook.com>
2024-05-16 17:16:57 +0800

    inject wordRepository

--- src/kfcservice/src/main/java/io/geextudio/kfcservice/KfcserviceApplication.java
+++ src/kfcservice/src/main/java/io/geextudio/kfcservice/KfcserviceApplication.java
@@ -4,8 +4,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.geextudio.kfcservice.domain.WordRepository;
+
 @SpringBootApplication
 public class KfcserviceApplication implements CommandLineRunner {
+
+	private final WordRepository wordRepository;
+
+	public KfcserviceApplication(WordRepository wordRepository) {
+	    this.wordRepository = wordRepository;
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
