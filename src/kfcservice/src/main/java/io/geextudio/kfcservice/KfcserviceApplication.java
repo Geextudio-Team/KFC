98c19c384fa00b2c124022a2646ff34a0cb1fdb9
Yang SHEN <geextudio@outlook.com>
2024-05-22 17:21:26 +0800

    logger

--- src/kfcservice/src/main/java/io/geextudio/kfcservice/KfcserviceApplication.java
+++ src/kfcservice/src/main/java/io/geextudio/kfcservice/KfcserviceApplication.java
@@ -1,5 +1,7 @@
 package io.geextudio.kfcservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,8 @@ import io.geextudio.kfcservice.domain.WordRepository;
 
 @SpringBootApplication
 public class KfcserviceApplication implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(KfcserviceApplication.class);
 
 	private final WordRepository wordRepository;
 
@@ -20,6 +24,8 @@ public class KfcserviceApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("Hello Word!");
 		wordRepository.save(new Word("Hello", "你好", "int", "Hello Word!"));
+
+		logger.info("Word: {} saved", "Hello");
 	}
 
 	// @Bean
